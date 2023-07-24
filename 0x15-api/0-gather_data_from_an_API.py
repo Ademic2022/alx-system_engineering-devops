8eff94529f68ab913bb42bb21fba7e90f2841472
ademic <adeeyomichael77@gmail.com>
2023-07-24 10:34:12 -0700

    gather data form an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ def get_todo_list_progress(employee_id):
     user = requests.get(url + "users/{}".format(employee_id)).json()
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [t.get("title") for t in todos if t.get("completed")]
     return user.get("name"), len(completed), len(todos), completed
 
 
@@ -19,9 +19,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    name, num_completed, total_tasks, completed_tasks = get_todo_list_progress(employee_id)
+    name, num_completed, total_tasks,
+    completed_tasks = get_todo_list_progress(employee_id)
 
-    print("Employee {} is done with tasks({}/{}):".format(name, num_completed, total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, num_completed, total_tasks))
     for task in completed_tasks:
         print("\t" + task)
-
